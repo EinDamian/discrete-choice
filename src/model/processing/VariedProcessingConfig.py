7161283ebd76b4e28b2e0e67ffe39573856ef024
Michael Schur <michael.schur@student.kit.edu>
2023-07-06 19:37:29 +0200

    Changed "components" field of VariedProcessingConfig to an implicit property. There is no need for a own field, because the components of the varied process are already defined in the config settings.

--- src/model/processing/VariedProcessingConfig.py
+++ src/model/processing/VariedProcessingConfig.py
@@ -6,6 +6,7 @@ from src.model.processing.ProcessingConfig import ProcessingConfig
 from src.model.processing.SimpleProcessingConfig import SimpleProcessingConfig
 from src.model.processing.Evaluation import Evaluation
 
+from functools import cached_property
 import pandas as pd
 
 
@@ -13,19 +14,18 @@ import pandas as pd
 class VariedProcessingConfig(ProcessingConfig):
     __DISPLAY_NAME = 'Varied Maximum-Likelihood Estimation (Biogeme)'
 
-    components: list[SimpleProcessingConfig] = field(default_factory=list)
-
     def process(self, model: Model) -> Evaluation:
         single_result_gen = map(lambda c: c.process(model).result, self.components)  # single result generator
         result = pd.concat(single_result_gen, axis=1, keys=range(len(self.components)))  # concat all single results together to one DataFrame
         return Evaluation(result)
+
+    @cached_property
+    def components(self) -> list[SimpleProcessingConfig]:
+        raise NotImplementedError  # TODO: IMPLEMENTIEREN
 
     @property
     def display_name(self) -> str:
         return VariedProcessingConfig.__DISPLAY_NAME
 
     def set_settings(self, settings: pd.DataFrame) -> VariedProcessingConfig:
-        return VariedProcessingConfig(settings, self.components)
-
-    def set_components(self, components: list[SimpleProcessingConfig]) -> VariedProcessingConfig:
-        return VariedProcessingConfig(self.settings, components)
+        return VariedProcessingConfig(settings)
