1b8d5d1edd5cd7d5861166aca0e93b3e3d11b686
Kevin2471 <k_power2406@yahoo.de>
2023-07-11 02:19:39 +0200

    Start of Combobox in ProcessingWidget

--- src/view/ProcessingWidget.py
+++ src/view/ProcessingWidget.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 import os
 
-from PyQt5.QtWidgets import QWidget, QTreeWidgetItem
+from PyQt5.QtWidgets import QWidget, QTreeWidgetItem, QComboBox
 from PyQt5 import uic
 
 from src.controller.calculation.ConfigurationController import ConfigurationController
+
 
 class ProcessingWidget(QWidget):
     def __init__(self, parent=None):
@@ -13,6 +14,10 @@ class ProcessingWidget(QWidget):
         uic.loadUi(f'{os.path.dirname(__file__)}/ui/processing_info.ui', self)  # load ui file created with Qt Creator
 
         self.__controller: ConfigurationController = ConfigurationController()
+        combo_box = self.findChild(QComboBox, "combo_process_type")
+        config_names = self.__controller.get_project().get_config_display_names()
+        for name in config_names:
+            combo_box.addItem(name)
 
     def update(self):
         raise NotImplementedError  # TODO: IMPLEMENTIEREN
