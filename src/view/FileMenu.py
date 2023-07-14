76669ffd76cf47f8297b2d270b219cb2e7b84dbd
Alissa S <118069263+alice221927@users.noreply.github.com>
2023-07-14 18:24:06 +0200

    added export option in file menu; implemented the export option; changed all file dialogs to NonNativeFileDialog: added Documentation to FileMenu; All functions in FileMenu are connected to the controller

--- src/view/FileMenu.py
+++ src/view/FileMenu.py
@@ -8,56 +8,106 @@ from src.controller.ProjectManager import ProjectManager
 
 
 class FileMenu(Menu):
-    def __init__(self, parent=None):
+    """
+    This class represents the file menu in the main window. It enables the user to manage the project
+    (create, open, save) and to import and export the data (csv contains survey data and derivatives)
+    """
+    def __init__(self, parent=None):    # parent should be MenuBar
+        """
+        initializes a file menu by getting the GUI-Design and
+        enabling the functionalities of the buttons
+        @param parent: the parent of file menu
+        @type parent: QWidget
+        """
         super().__init__(parent)
 
         self.__project_manager: ProjectManager = ProjectManager()
 
         ui_file_menu = self.parent().findChild(QMenu, "menu_file")
 
-        new_project_button = UIUtil.get_action(ui_file_menu, 'action_new_project')
-        new_project_button.triggered.connect(self.open_new_project)
-        import_data_button = UIUtil.get_action(ui_file_menu, 'action_import_data')
-        import_data_button.triggered.connect(self.import_data)
-        open_project_button = UIUtil.get_action(ui_file_menu, 'action_project_open')
-        open_project_button.triggered.connect(self.open_project)
-        save_project_button = UIUtil.get_action(ui_file_menu, 'action_project_save')
-        save_project_button.triggered.connect(self.save_project)
-        save_as_button = UIUtil.get_action(ui_file_menu, 'action_project_save_as')
-        save_as_button.triggered.connect(self.save_project_as)
+        self.new_project_button = UIUtil.get_action(ui_file_menu, 'action_new_project')
+        self.new_project_button.triggered.connect(self.open_new_project)
+        self.open_project_button = UIUtil.get_action(ui_file_menu, 'action_project_open')
+        self.open_project_button.triggered.connect(self.open_project)
+        self.save_project_button = UIUtil.get_action(ui_file_menu, 'action_project_save')
+        self.save_project_button.triggered.connect(self.save_project)
+        self.save_as_button = UIUtil.get_action(ui_file_menu, 'action_project_save_as')
+        self.save_as_button.triggered.connect(self.save_project_as)
+        self.import_data_button = UIUtil.get_action(ui_file_menu, 'action_import_data')
+        self.import_data_button.triggered.connect(self.import_data)
+        self.export_data_button = UIUtil.get_action(ui_file_menu, 'action_export_data')
+        self.export_data_button.triggered.connect(self.export_data)
 
     def open_project(self):
-        user_input = QFileDialog().getExistingDirectory(self, 'Open Project', 'Directory (*.dir)',
-                                                        options=QFileDialog.ShowDirsOnly)
-        if user_input:
-            self.__project_manager.open(user_input[0])
+        """
+        Enables the user to open a project, which already exists.
+        A File Dialog will be shown, where the user can only choose a directory,
+        because a project can only be saved as directory
+        """
+        dlg = QFileDialog()
+        dlg.setOption(QFileDialog.DontUseNativeDialog)
+        dlg.setWindowTitle("Open Project")
+        dlg.setFileMode(QFileDialog.DirectoryOnly)
+        dlg.setNameFilter("Directory (*.dir)")
+        dlg.exec_()
+        dlg.fileSelected.connect(self.__project_manager.open)
 
     def open_new_project(self):
-        # TODO: self.save_project()
+        """
+        Enables the user to open a new project. The user can enter the project's path
+        as soon as he tries to save it
+        @return:
+        @rtype:
+        """
+        # TODO: self.save_project() ?
         self.__project_manager.new()
 
     def save_project(self):
+        """
+        enables th user to save the opened project.
+        In case the project is new and hasn't been saved before, a
+        file dialog will be shown for choosing a path to save to.
+        Otherwise, save is performed automatically.
+        """
         if self.__project_manager.get_project().path is None:
-            path = QFileDialog().getExistingDirectory(self, 'Open Project', 'Directory (*.dir)',
-                                                      options=QFileDialog.ShowDirsOnly)
-            self.__project_manager.get_project().path = path
-
-        self.__project_manager.save(None)
+            options = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            user_input = QFileDialog.getSaveFileName(self, 'Save File', '', 'Directory (*.dir)', options=options)
+            if user_input:
+                # self.__project_manager.get_project().path = user_input[0] #TODO need a setter in controller?
+                self.__project_manager.save(None)
+        else:
+            self.__project_manager.save(None)
 
     def save_project_as(self):
-        user_input = QFileDialog.getSaveFileName(self, 'Save File', '', 'Directory (*.dir)', )
+        """
+        enables the user to save the project in a path, that differs from the project's original path
+        """
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        user_input = QFileDialog.getSaveFileName(self, 'Save Project As', '', 'Directory (*.dir)', options=options)
         if user_input:
-            self.__project_manager.save(user_input[0])  # contains the path
+            self.__project_manager.save(user_input[0])  # user_input[0] contains the path
 
     def import_data(self):
+        """
+        Using this option in FileMenu, the user can import the survey data, which are stored in a csv file.
+        """
         dlg = QFileDialog()
-        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setOption(QFileDialog.DontUseNativeDialog)
+        dlg.setWindowTitle("Import Data")
+        dlg.setFileMode(QFileDialog.ExistingFile)
         dlg.setNameFilter("CSV File (*.csv)")
-        dlg.fileSelected.connect(self.__project_manager.import_)
         dlg.exec_()
+        dlg.fileSelected.connect(self.__project_manager.import_)
 
-    # what is this function for???
-    def export_data(self):  # How to specify csv or JSON?
-        user_input = QFileDialog.getSaveFileName(self, 'Export File', '', 'Directory (*.dir)', )
-        name = user_input[0] + '.csv'
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
+    def export_data(self):  # TODO how to specify file type? csv, JSON?
+        """
+        This option enables the user to export the survey data and their derivatives to a path of  his choice.
+        In the file dialog the user can name the exported file as he wishes.
+        """
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        user_input = QFileDialog.getSaveFileName(self, 'Export to', '', 'Directory (*.dir)', options=options)
+        if user_input:
+            self.__project_manager.export(user_input[0])  # user_input[0] contains the path
