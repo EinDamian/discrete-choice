d7af632089fbf95418e2af772b606d48e64fe81b
Alissa S <118069263+alice221927@users.noreply.github.com>
2023-07-20 13:08:05 +0200

    worked on comments and Docs

--- src/view/EvaluationWidget.py
+++ src/view/EvaluationWidget.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import os
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QFileDialog, QTableView
+from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QTableView
 from PyQt5 import uic
 
 from src.controller.calculation.EvaluationController import EvaluationController
