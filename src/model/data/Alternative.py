f8ac80ca6e0c293df4e78377cf2069b97b001a60
Damian Reich <damian-reich@web.de>
2023-07-20 14:15:39 +0200

    Finished up all docstring in the data package.

--- src/model/data/Alternative.py
+++ src/model/data/Alternative.py
@@ -6,5 +6,13 @@ from src.model.data.functions.FunctionalExpression import FunctionalExpression
 
 @dataclass(frozen=True)
 class Alternative:
+    """
+    Represents an alternative.
+    Extends a functional expression with an additional availability condition.
+
+    Attributes:
+        function: Functional expression of the alternative.
+        availability_condition: Availability of the alternative.
+    """
     function: FunctionalExpression
     availability_condition: FunctionalExpression
