6c10d3aa2adeb60aafb93ca438ee622de975ad53
Michael Schur <michael.schur@student.kit.edu>
2023-07-21 19:48:34 +0200

    Autosave fix

--- src/controller/AbstractController.py
+++ src/controller/AbstractController.py
@@ -13,8 +13,10 @@ class AbstractController:
     they cannot modify it or use its functions. 
     It serves as a colleague to the ProjectManager in the 'Mediator' design pattern.
     """
+
     def __init__(self):
         self.__project_manager: ProjectManager = ProjectManager()
+        self.__saving_thread = None
 
     def get_project(self) -> Project:
         """Method as an interface between the controllers and the Project from the Model package.
@@ -23,9 +25,12 @@ class AbstractController:
             Project: The current project snapshot.
         """
         return self.__project_manager.get_project()
-    
+
     def save(self):
         """Method used to initiate the saving process in a different thread after every step that changes the model.
         """
-        x = threading.Thread(target=self.__project_manager.save, args=(), daemon=False)
-        x.start()
+        if self.__saving_thread is not None and self.__saving_thread.is_alive():
+            return  # skip autosave if autosave thread is still running
+
+        self.__saving_thread = threading.Thread(target=self.__project_manager.save, args=(), daemon=False)
+        self.__saving_thread.start()
