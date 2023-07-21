48285d86ae231deb4c695842f2c57a0f651c525b
Alissa S <118069263+alice221927@users.noreply.github.com>
2023-07-21 10:41:36 +0200

    removed all actions except undo and redo

--- src/view/EditMenu.py
+++ src/view/EditMenu.py
@@ -31,40 +31,6 @@ class EditMenu(Menu):
 
         self.undo_button = UIUtil.get_action(ui_edit_menu, 'action_undo')
         self.undo_button.triggered.connect(self.undo)
-        '''
-        Remove these functions from edit menu?
-        
-        self.copy_button = UIUtil.get_action(ui_edit_menu, 'action_copy')
-        self.copy_button.triggered.connect(self.copy)
-        self.cut_button = UIUtil.get_action(ui_edit_menu, 'action_cut')
-        self.cut_button.triggered.connect(self.cut)
-        self.delete_button = UIUtil.get_action(ui_edit_menu, 'action_delete')
-        self.delete_button.triggered.connect(self.delete)
-        self.find_button = UIUtil.get_action(ui_edit_menu, 'action_find')
-        self.find_button.triggered.connect(self.find)
-        self.paste_button = UIUtil.get_action(ui_edit_menu, 'action_paste')
-        self.paste_button.triggered.connect(self.paste)
-        self.select_all_button = UIUtil.get_action(ui_edit_menu, 'action_select_all')
-        self.select_all_button.triggered.connect(self.select_all)
-        '''
-
-    def cut(self, content: str):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
-
-    def copy(self, content: str):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
-
-    def paste(self):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
-
-    def delete(self, content: str):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
-
-    def find(self, content: str):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
-
-    def select_all(self):
-        raise NotImplementedError  # TODO: IMPLEMENTIEREN
 
     def undo(self):
         """
