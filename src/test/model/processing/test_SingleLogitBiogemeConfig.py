5c38fe3a695fb43e841e5e9d0c94ee7d4277dba5
Michael Schur <michael.schur@student.kit.edu>
2023-08-11 15:25:02 +0200

    Improve unittests for SingleLogitBiogemeConfig

--- src/test/model/processing/test_SingleLogitBiogemeConfig.py
+++ src/test/model/processing/test_SingleLogitBiogemeConfig.py
@@ -9,30 +9,36 @@ from src.model.data.Alternative import Alternative
 from src.model.data.functions.FunctionalExpression import FunctionalExpression
 
 import unittest
+from parameterized import parameterized
 import pandas as pd
 
 
 class TestSingleLogitBiogemeConfig(unittest.TestCase):
-    def test_b01logit(self):
-        """
-        Example for using biogeme.
-        Source: https://github.com/michelbierlaire/biogeme/blob/master/examples/swissmetro/b01logit.py (06.07.2023)
-        """
+    @staticmethod
+    def __swissmetro_raw_data():
+        raw_data = pd.read_csv(f'{os.path.dirname(__file__)}/../../resources/swissmetro.csv', sep='\t')
+        raw_data = raw_data.drop(
+            raw_data[((raw_data['PURPOSE'] != 1) * (raw_data['PURPOSE'] != 3) + (raw_data['CHOICE'] == 0)) > 0].index)
+        return raw_data
 
-        derivatives = {
-            'SM_COST': FunctionalExpression('SM_CO * (GA == 0)'),
-            'TRAIN_COST': FunctionalExpression('TRAIN_CO * (GA == 0)'),
-            'CAR_AV_SP': FunctionalExpression('CAR_AV * (SP != 0)'),
-            'TRAIN_AV_SP': FunctionalExpression('TRAIN_AV * (SP != 0)'),
-            'TRAIN_TT_SCALED': FunctionalExpression('TRAIN_TT / 100'),
-            'TRAIN_COST_SCALED': FunctionalExpression('TRAIN_COST / 100'),
-            'SM_TT_SCALED': FunctionalExpression('SM_TT / 100'),
-            'SM_COST_SCALED': FunctionalExpression('SM_COST / 100'),
-            'CAR_TT_SCALED': FunctionalExpression('CAR_TT / 100'),
-            'CAR_CO_SCALED': FunctionalExpression('CAR_CO / 100'),
-        }
+    @staticmethod
+    def __ifvrabus_raw_data():
+        return pd.read_csv(f'{os.path.dirname(__file__)}/../../resources/Choicedata.csv', sep=';')
 
-        alternatives = {
+    @parameterized.expand([
+        ('b01logit', Model(Data(raw_data=__swissmetro_raw_data(), raw_data_path=None,
+                                derivatives={
+                                    'SM_COST': FunctionalExpression('SM_CO * (GA == 0)'),
+                                    'TRAIN_COST': FunctionalExpression('TRAIN_CO * (GA == 0)'),
+                                    'CAR_AV_SP': FunctionalExpression('CAR_AV * (SP != 0)'),
+                                    'TRAIN_AV_SP': FunctionalExpression('TRAIN_AV * (SP != 0)'),
+                                    'TRAIN_TT_SCALED': FunctionalExpression('TRAIN_TT / 100'),
+                                    'TRAIN_COST_SCALED': FunctionalExpression('TRAIN_COST / 100'),
+                                    'SM_TT_SCALED': FunctionalExpression('SM_TT / 100'),
+                                    'SM_COST_SCALED': FunctionalExpression('SM_COST / 100'),
+                                    'CAR_TT_SCALED': FunctionalExpression('CAR_TT / 100'),
+                                    'CAR_CO_SCALED': FunctionalExpression('CAR_CO / 100'),
+                                }), alternatives={
             'alt1': Alternative(
                 FunctionalExpression('ASC_TRAIN + B_TIME * TRAIN_TT_SCALED + B_COST * TRAIN_COST_SCALED'),
                 availability_condition=FunctionalExpression('TRAIN_AV_SP'),
@@ -45,20 +51,166 @@ class TestSingleLogitBiogemeConfig(unittest.TestCase):
                 FunctionalExpression('ASC_CAR + B_TIME * CAR_TT_SCALED + B_COST * CAR_CO_SCALED'),
                 availability_condition=FunctionalExpression('CAR_AV_SP'),
                 choice_idx=3)
-        }
+        }, choice=FunctionalExpression('CHOICE'))),
 
-        choice = FunctionalExpression('CHOICE')
-
-        raw_data = pd.read_csv(f'{os.path.dirname(__file__)}/../../resources/swissmetro.csv', sep='\t')
-        raw_data = raw_data.drop(
-            raw_data[((raw_data['PURPOSE'] != 1) * (raw_data['PURPOSE'] != 3) + (raw_data['CHOICE'] == 0)) > 0].index)
-        data = Data(raw_data, None, derivatives)
-        model = Model(data, alternatives, choice)
+        ('ifv_rabus',
+         Model(Data(raw_data=__ifvrabus_raw_data(), raw_data_path=None, derivatives={}),
+               alternatives={
+                   'fuss': Alternative(
+                       FunctionalExpression(
+                           'asc_fuss + b_arbeit_on_fuss * (Wegezweck == 1) + b_freizeit_on_fuss * (Wegezweck == 4) + fz_fuss * bfz_fuss'),
+                       availability_condition=FunctionalExpression('av_fuss'),
+                       choice_idx=1),
+                   'oev_fuss': Alternative(
+                       FunctionalExpression(
+                           'asc_oev_fuss + b_arbeit_on_oev_fuss * (Wegezweck == 1) + b_freizeit_on_oev_fuss * (Wegezweck == 4) + bfz_oev * fz_oev_fuss + bcost_oev * cost_oev_fuss + bfreq_oev * freq_oev_fuss + bfz_fuss2oev * zugang_oev_fuss'),
+                       availability_condition=FunctionalExpression('av_oev_fuss'),
+                       choice_idx=6),
+                   'rad': Alternative(
+                       FunctionalExpression(
+                           'asc_rad + b_arbeit_on_rad * (Wegezweck == 1) + b_freizeit_on_rad * (Wegezweck == 4) + bfz_rad * fz_rad'),
+                       availability_condition=FunctionalExpression('av_rad'),
+                       choice_idx=2),
+                   'car': Alternative(
+                       FunctionalExpression(
+                           'asc_car + b_arbeit_on_car * (Wegezweck == 1) + b_freizeit_on_car * (Wegezweck == 4) + bfz_car * fz_car'),
+                       availability_condition=FunctionalExpression('av_car'),
+                       choice_idx=4),
+                   'shuttle_tb': Alternative(
+                       FunctionalExpression(
+                           'asc_shuttle_tb + b_oek1_on_shuttle_tb * (oek_status == 1) + b_arbeit_on_shuttle_tb * (Wegezweck == 1) + b_freizeit_on_shuttle_tb * (Wegezweck == 4) + bfz_shuttle * fz_shuttle_tb'),
+                       availability_condition=FunctionalExpression('av_shuttle_tb'),
+                       choice_idx=13),
+                   'shuttle_od': Alternative(
+                       FunctionalExpression(
+                           'asc_shuttle_od + b_oek1_on_shuttle_od * (oek_status == 1) + b_arbeit_on_shuttle_od * (Wegezweck == 1) + b_freizeit_on_shuttle_od * (Wegezweck == 4) + bfz_shuttle * fz_shuttle_od'),
+                       availability_condition=FunctionalExpression('av_shuttle_od'),
+                       choice_idx=14),
+                   'oev_rad': Alternative(
+                       FunctionalExpression(
+                           'asc_oev_rad + b_arbeit_on_oev_rad * (Wegezweck == 1) + b_freizeit_on_oev_rad * (Wegezweck == 4) + bfz_oev*Zeit_oev_rad'),
+                       availability_condition=FunctionalExpression('av_oev_rad'),
+                       choice_idx=7),
+                   'oev_car': Alternative(
+                       FunctionalExpression('asc_oev_car + bfz_oev*Zeit_oev_car'),
+                       availability_condition=FunctionalExpression('av_oev_car'),
+                       choice_idx=11),
+                   'oev_shuttle_tb': Alternative(
+                       FunctionalExpression(
+                           'asc_oev_shuttle_tb + b_freizeit_on_oev_shuttle_tb * (Wegezweck == 4) + bfz_oev * Zeit_oev_shuttle_tb'),
+                       availability_condition=FunctionalExpression('av_oev_shuttle_tb'),
+                       choice_idx=9),
+                   'oev_shuttle_od': Alternative(
+                       FunctionalExpression(
+                           'asc_oev_shuttle_od + b_freizeit_on_oev_shuttle_od * (Wegezweck == 4) + bfz_oev * Zeit_oev_shuttle_od'),
+                       availability_condition=FunctionalExpression('av_oev_shuttle_od'),
+                       choice_idx=10)
+               }, choice=FunctionalExpression('choice'))),
+    ])
+    def test_process(self, name: str, model: Model):
+        """
+        Example for using biogeme.
+        Source: https://github.com/michelbierlaire/biogeme/blob/master/examples/swissmetro/b01logit.py (06.07.2023)
+        """
         config = SingleLogitBiogemeConfig()
-
+        print(model.data.raw_data.columns)
         evaluation = config.process(model)
 
         self.assertEqual(type(evaluation.result), pd.DataFrame)
+
+    @parameterized.expand([
+        ('b01logit_missing_variable', KeyError,
+         Model(Data(raw_data=__swissmetro_raw_data(), raw_data_path=None,
+                    derivatives={
+                        'SM_COST': FunctionalExpression('SM_CO * (GA == 0)'),
+                        'CAR_AV_SP': FunctionalExpression('CAR_AV * (SP != 0)'),
+                        'TRAIN_AV_SP': FunctionalExpression('TRAIN_AV * (SP != 0)'),
+                        'TRAIN_TT_SCALED': FunctionalExpression('TRAIN_TT / 100'),
+                        'TRAIN_COST_SCALED': FunctionalExpression('abc / 100'),
+                        'SM_TT_SCALED': FunctionalExpression('SM_TT / 100'),
+                        'SM_COST_SCALED': FunctionalExpression('SM_COST / 100'),
+                        'CAR_TT_SCALED': FunctionalExpression('CAR_TT / 100'),
+                        'CAR_CO_SCALED': FunctionalExpression('CAR_CO / 100'),
+                    }), alternatives={
+             'alt1': Alternative(
+                 FunctionalExpression('ASC_TRAIN + B_TIME * TRAIN_TT_SCALED + B_COST * TRAIN_COST_SCALED'),
+                 availability_condition=FunctionalExpression('TRAIN_AV_SP'),
+                 choice_idx=1),
+             'alt2': Alternative(
+                 FunctionalExpression('ASC_SM + B_TIME * SM_TT_SCALED + B_COST * SM_COST_SCALED'),
+                 availability_condition=FunctionalExpression('SM_AV'),
+                 choice_idx=2),
+             'alt3': Alternative(
+                 FunctionalExpression('ASC_CAR + B_TIME * CAR_TT_SCALED + B_COST * CAR_CO_SCALED'),
+                 availability_condition=FunctionalExpression('CAR_AV_SP'),
+                 choice_idx=3)
+         }, choice=FunctionalExpression('CHOICE'))),
+
+        ('b01logit_cyclic_definition', KeyError,
+         Model(Data(raw_data=__swissmetro_raw_data(), raw_data_path=None,
+                    derivatives={
+                        'SM_COST': FunctionalExpression('SM_CO * (GA == 0)'),
+                        'TRAIN_COST': FunctionalExpression('TRAIN_COST_SCALED * (GA == 0)'),
+                        'CAR_AV_SP': FunctionalExpression('CAR_AV * (SP != 0)'),
+                        'TRAIN_AV_SP': FunctionalExpression('TRAIN_AV * (SP != 0)'),
+                        'TRAIN_TT_SCALED': FunctionalExpression('TRAIN_TT / 100'),
+                        'TRAIN_COST_SCALED': FunctionalExpression('TRAIN_COST / 100'),
+                        'SM_TT_SCALED': FunctionalExpression('SM_TT / 100'),
+                        'SM_COST_SCALED': FunctionalExpression('SM_COST / 100'),
+                        'CAR_TT_SCALED': FunctionalExpression('CAR_TT / 100'),
+                        'CAR_CO_SCALED': FunctionalExpression('CAR_CO / 100'),
+                    }), alternatives={
+             'alt1': Alternative(
+                 FunctionalExpression('ASC_TRAIN + B_TIME * TRAIN_TT_SCALED + B_COST * TRAIN_COST_SCALED'),
+                 availability_condition=FunctionalExpression('TRAIN_AV_SP'),
+                 choice_idx=1),
+             'alt2': Alternative(
+                 FunctionalExpression('ASC_SM + B_TIME * SM_TT_SCALED + B_COST * SM_COST_SCALED'),
+                 availability_condition=FunctionalExpression('SM_AV'),
+                 choice_idx=2),
+             'alt3': Alternative(
+                 FunctionalExpression('ASC_CAR + B_TIME * CAR_TT_SCALED + B_COST * CAR_CO_SCALED'),
+                 availability_condition=FunctionalExpression('CAR_AV_SP'),
+                 choice_idx=3)
+         }, choice=FunctionalExpression('CHOICE'))),
+
+        ('b01logit_syntax', SyntaxError,
+         Model(Data(raw_data=__swissmetro_raw_data(), raw_data_path=None,
+                    derivatives={
+                        'SM_COST': FunctionalExpression('SM_CO ****** (GA == 0)'),
+                        'TRAIN_COST': FunctionalExpression('TRAIN_CO * (GA == 0)'),
+                        'CAR_AV_SP': FunctionalExpression('CAR_AV * (SP != 0)'),
+                        'TRAIN_AV_SP': FunctionalExpression('TRAIN_AV * (SP != 0)'),
+                        'TRAIN_TT_SCALED': FunctionalExpression('TRAIN_TT / 100'),
+                        'TRAIN_COST_SCALED': FunctionalExpression('TRAIN_COST / 100'),
+                        'SM_TT_SCALED': FunctionalExpression('SM_TT / 100'),
+                        'SM_COST_SCALED': FunctionalExpression('SM_COST / 100'),
+                        'CAR_TT_SCALED': FunctionalExpression('CAR_TT / 100'),
+                        'CAR_CO_SCALED': FunctionalExpression('CAR_CO / 100'),
+                    }), alternatives={
+             'alt1': Alternative(
+                 FunctionalExpression('ASC_TRAIN + B_TIME * TRAIN_TT_SCALED + B_COST * TRAIN_COST_SCALED'),
+                 availability_condition=FunctionalExpression('TRAIN_AV_SP'),
+                 choice_idx=1),
+             'alt2': Alternative(
+                 FunctionalExpression('ASC_SM + B_TIME * SM_TT_SCALED + B_COST * SM_COST_SCALED'),
+                 availability_condition=FunctionalExpression('SM_AV'),
+                 choice_idx=2),
+             'alt3': Alternative(
+                 FunctionalExpression('ASC_CAR + B_TIME * CAR_TT_SCALED + B_COST * CAR_CO_SCALED'),
+                 availability_condition=FunctionalExpression('CAR_AV_SP'),
+                 choice_idx=3)
+         }, choice=FunctionalExpression('CHOICE'))),
+    ])
+    def test_error_process(self, name: str, expected_exception, model: Model):
+        """
+        Example for using biogeme.
+        Source: https://github.com/michelbierlaire/biogeme/blob/master/examples/swissmetro/b01logit.py (06.07.2023)
+        """
+        config = SingleLogitBiogemeConfig()
+
+        with self.assertRaises(expected_exception):
+            config.process(model)
 
     @staticmethod
     def __example__b01logit() -> Evaluation:
@@ -66,14 +218,14 @@ class TestSingleLogitBiogemeConfig(unittest.TestCase):
         Example for using biogeme.
         Source: https://github.com/michelbierlaire/biogeme/blob/master/examples/swissmetro/b01logit.py (06.07.2023)
         """
-        #%%
+        # %%
         import biogeme.biogeme as bio
         from biogeme import models
         from biogeme.expressions import Beta
         import pandas as pd
         import biogeme.database as db
         from biogeme.expressions import Variable
-        #%%
+        # %%
         # Read the data
         df = pd.read_csv('src/test/resources/swissmetro.dat', sep='\t')
         database = db.Database('swissmetro', df)
@@ -117,7 +269,7 @@ class TestSingleLogitBiogemeConfig(unittest.TestCase):
         SM_COST_SCALED = database.DefineVariable('SM_COST_SCALED', SM_COST / 100)
         CAR_TT_SCALED = database.DefineVariable('CAR_TT_SCALED', CAR_TT / 100)
         CAR_CO_SCALED = database.DefineVariable('CAR_CO_SCALED', CAR_CO / 100)
-        #%%
+        # %%
         # Parameters to be estimated
         ASC_CAR = Beta('ASC_CAR', 0, None, None, 0)
         ASC_TRAIN = Beta('ASC_TRAIN', 0, None, None, 0)
@@ -140,7 +292,7 @@ class TestSingleLogitBiogemeConfig(unittest.TestCase):
 
         choice = database.variables['CHOICE']
 
-        #%%
+        # %%
         prop = models.logit(dict(enumerate(v, 1)), availability_conditions, choice)
 
         # Create the Biogeme object
@@ -149,7 +301,7 @@ class TestSingleLogitBiogemeConfig(unittest.TestCase):
         bio_model.modelName = 'biogeme_model'  # set model name to prevent warning from biogeme
         result = bio_model.estimate()
         print(result.getEstimatedParameters())
-        #%%
+        # %%
         return Evaluation(result.getEstimatedParameters())
 
 
