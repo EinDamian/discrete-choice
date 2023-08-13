b2e8803e75808bae423bfbc3a8935d8c14057c91
uspvq <uspvq@student.kit.edu>
2023-08-13 10:52:01 +0200

    applied code formatting

--- src/view/ConfirmationDialog.py
+++ src/view/ConfirmationDialog.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QMessageBox, QWidget
 
 class ConfirmationDialog:
     """Class that offers Confirmation Dialogs"""
-    
+
     def confirm(self, parent: QWidget, msg: str) -> bool:
         """Gets Confirmation of the Message from the User.
 
@@ -14,7 +14,8 @@ class ConfirmationDialog:
         Returns:
             bool: True if the User gave Confirmation, else False.
         """
-        reply = QMessageBox.question(parent, None, msg, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        reply = QMessageBox.question(
+            parent, None, msg, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
             return True
         else:
