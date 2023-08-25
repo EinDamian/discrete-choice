2848bf7e07415250db8e9beb1c9bc8bb17027864
uspvq <uspvq@student.kit.edu>
2023-08-25 23:47:27 +0200

    remove print statement

--- src/view/FunctionHighlightDelegate.py
+++ src/view/FunctionHighlightDelegate.py
@@ -67,7 +67,6 @@ class FunctionHighlightDelegate(QStyledItemDelegate):
             return QRect(text_rect.left(), text_rect.top(), ConfigFunctionHighlighting.MISSING_EXPRESSION_HIGHLIGHTING_WIDTH + text_rect.right(), text_rect.height())
         # if the marking has no length the first character after the sign will be highlighted
         elif end - start == 0:
-            print(end, start)
             end += 1
 
         start_x = text_rect.left() + fm.horizontalAdvance(text[:start]) + ConfigFunctionHighlighting.HIGHLIGHTING_OFFSET
