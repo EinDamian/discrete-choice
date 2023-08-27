09620c7c08db340d9c10372cd47e446f6a0e179c
uspvq <uspvq@student.kit.edu>
2023-08-27 23:30:04 +0200

    remove unnecessary imports

--- src/view/EvaluationWidget.py
+++ src/view/EvaluationWidget.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import os
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QTableView, QDialog, QLabel, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QTableView, QMessageBox
 from PyQt5 import uic
 
 from src.controller.calculation.EvaluationController import EvaluationController
